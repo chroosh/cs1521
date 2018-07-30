d4f9524e787baad2e0f90cacea33c68b6c5bde73
z5165244 <z5165244@cse.unsw.edu.au>
2018-07-31 09:18:45 +1000

    lab02

---
+++ lab02/BigNum.c
@@ -0,0 +1,38 @@
+// BigNum.h ... LARGE positive integer values
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+#include "BigNum.h"
+
+// Initialise a BigNum to N bytes, all zero
+void initBigNum(BigNum *n, int Nbytes)
+{
+   // TODO
+   return;
+}
+
+// Add two BigNums and store result in a third BigNum
+void addBigNums(BigNum n, BigNum m, BigNum *res)
+{
+   // TODO
+   return;
+}
+
+// Set the value of a BigNum from a string of digits
+// Returns 1 if it *was* a string of digits, 0 otherwise
+int scanBigNum(char *s, BigNum *n)
+{
+   // TODO
+   return 1;
+}
+
+// Display a BigNum in decimal format
+void showBigNum(BigNum n)
+{
+   // TODO
+   return;
+}
+
