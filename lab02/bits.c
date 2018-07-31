ddda9e1e516262629399ff4ec8546defacebc7f9
Christopher Shi <christopher.shi98@gmail.com>
2018-08-01 04:01:35 +1000

    lab02

--- lab02/bits.c
+++ lab02/bits.c
@@ -6,33 +6,33 @@ int main()
 {
 	// Code to generate and display the largest "int" value
 
-	int x = ??;
-	printf("int %?, %?\n", x, x);
+	int x = 2147483647;
+	printf("int %x, %d\n", x, x);
 
 	// Code to generate and display the largest "unsigned int" value
 
-	unsigned int y = ??;
-	printf("unsigned int %?, %?\n", y, y);
+	unsigned int y = 4294967295;
+	printf("unsigned int %x, %u\n", y, y);
 
 	// Code to generate and display the largest "long int" value
 
-	long int xx = ??;
-	printf("long int %?, %?\n", xx, xx);
+	long int xx = 2147483647;;
+	printf("long int %lx, %ld\n", xx, xx);
 
 	// Code to generate and display the largest "unsigned long int" value
 
-	unsigned long int xy = ??;
-	printf("unsigned long int %?, %?\n", xy, xy);
+	unsigned long int xy = 4294967295;;
+	printf("unsigned long int %lx, %lu\n", xy, xy);
 
 	// Code to generate and display the largest "long long int" value
 
-	long long int xxx = ??;
-	printf("long long int %?, %?\n", xxx, xxx);
+	long long int xxx = 9223372036854775807;
+	printf("long long int %llx, %lld\n", xxx, xxx);
 
 	// Code to generate and display the largest "unsigned long long int" value
 
-	unsigned long long int xxy = ??;
-	printf("unsigned long long int %?, %?\n", xxy, xxy);
+	unsigned long long int xxy = 1844674407370955161;
+	printf("unsigned long long int %llx, %llu\n", xxy, xxy);
 
 	return 0;
 }
